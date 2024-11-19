619c0d34486f2d42273f1d029a60aa4080dfc482
Maksim <mrazduev@gmail.com>
2024-11-19 20:49:37 +0300

    Module 4.1. Task 3. Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character >= 'A' && character <= 'z') {
+            char lowCharacter = Character.toLowerCase(character);
+
+            if (lowCharacter == 'a' || lowCharacter == 'e' || lowCharacter == 'u' || lowCharacter == 'o' || lowCharacter == 'i')
+                System.out.println("Vowel");
+            else
+                System.out.println("Consonant");
+        }
+        else
+            System.out.println("wrong alphabet!");
     }
 }
